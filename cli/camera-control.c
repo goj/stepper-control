7565629061f17a853f8ea98d47fac0825307c6bd
Krzysztof Goj <krzysztof.goj@gmail.com>
2013-05-19 22:33:13 +0200

    CLI: smoother turning

--- cli/camera-control.c
+++ cli/camera-control.c
@@ -25,7 +25,7 @@ void usage(char *name) {
     fprintf(stderr, "usage:\n");
     fprintf(stderr, "  %s help ......... print this message and exit.\n", name);
     fprintf(stderr, "  %s off .......... turn off the stepper engines.\n", name);
-    fprintf(stderr, "  %s turn dx dy ... turn the camera by (dx, dy) and hold.\n", name);
+    fprintf(stderr, "  %s turn X Y ... turn the camera by (X, Y) and hold.\n", name);
 }
 
 #ifdef DEBUG
@@ -64,47 +64,64 @@ int idx_add(int idx, int dir) {
     return idx + dir;
 }
 
-void turn_camera(usb_dev_handle *handle, int dx, int dy) {
+void turn_camera(usb_dev_handle *handle, int X, int Y) {
     int x_idx, y_idx;
     get_xy_indices(handle, &x_idx, &y_idx);
 
-    // make x and y non-negative and always grow (dx,dy > 0),
+    // make x and y non-negative and always grow (X,Y > 0),
     // reflect true direction in *_dir variables.
     int x = 0, y = 0;
     int x_dir = INC, y_dir = INC;
-    if (dx < 0) {
+    if (X < 0) {
         x_dir = DEC;
-        dx = -dx;
+        X = -X;
     }
-    if (dy < 0) {
+    if (Y < 0) {
         y_dir = DEC;
-        dy = -dy;
+        Y = -Y;
     }
 
-    // Now (x,y) grows from (0,0) towards (dx,dy).
-    // If (y/x > dy/dx), then we are over the intended
+    // Now (x,y) grows from (0,0) towards (X,Y).
+    // If (y/x > Y/X), then we are over the intended
     // slope and we should increase x in the next step.
-    // sign(y/x - dy/dx) == sign(y*dx - x*dy)
-    // Let's call (y*dx - x*dy) slope_error and update
+    //
+    // Note that y/x > Y/X <=> y/x - Y/X > 0
+    // and sign(y/x - Y/X) == sign(y*X - x*Y)
+    // Let's call (y*X - x*Y) slope_err and update
     // it in each step.
     int slope_err = 0;
+
+    // As we want the angular speed of each stepper to be
+    // constant we choose to sleep on the x change if X>Y
+    // and on the y change otherwise. This way steps in
+    // the "less popular" direction occur "in parallel"
+    // with the steps in "more popular" one.
+    int x_sleep, y_sleep;
+    if (X > Y) {
+        x_sleep = USLEEP_TIME * (X+Y) / X;
+        y_sleep = 0;
+    } else {
+        y_sleep = USLEEP_TIME * (X+Y) / Y;
+        x_sleep = 0;
+    }
     for (;;) {
-        if (x < dx && slope_err >= 0) {
+        if (x < X && slope_err >= 0) {
             ++x;
-            slope_err -= dy;
+            slope_err -= Y;
             x_idx = idx_add(x_idx, x_dir);
             show_position('X', x_idx);
             stepper_set_x(handle, steps[x_idx]);
-        } else if (y < dy && slope_err <= 0) {
+            usleep(x_sleep);
+        } else if (y < Y && slope_err < 0) {
             ++y;
-            slope_err += dx;
+            slope_err += X;
             y_idx = idx_add(y_idx, y_dir);
             show_position('Y', y_idx);
             stepper_set_y(handle, steps[y_idx]);
+            usleep(y_sleep);
         } else {
             break;
         }
-        usleep(USLEEP_TIME);
     }
 }
 
